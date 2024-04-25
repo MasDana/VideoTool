7e72df77ba21bb006bae487460e65e3ed7b36164
MasDana <putudeni1414@gmail.com>
2024-04-25 12:13:53 +0800

    first commit

---
+++ src/BelajarTipeData/BelajarTipeData.java
@@ -0,0 +1,96 @@
+
+public class BelajarTipeData {
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println("======================================");
+        System.out.println("======== TIPE DATA DALAM JAVA ========");
+        System.out.println("======================================");
+        System.out.println("     ");
+        System.out.println("======================================");
+        System.out.println("========        Integer       ========");
+        System.out.println("======================================");
+        int i = 10;
+        System.out.println("Nilai dari integer i adalah          : " + i);
+        System.out.println("Nilai maksimal dari integer adalah   : " + Integer.MAX_VALUE);
+        System.out.println("Nilai minimal dari integer adalah    : " + Integer.MIN_VALUE);
+        System.out.println("Besar integer dalam Byte adalah      : " + Integer.BYTES + " bytes");
+        System.out.println("Besar integer dalam Bit adalah       : " + Integer.SIZE + " bit");
+
+        System.out.println("     ");
+        System.out.println("======================================");
+        System.out.println("========          Byte        ========");
+        System.out.println("======================================");
+        byte a = 10;
+        System.out.println("Nilai dari byte a adalah          : " + a);
+        System.out.println("Nilai maksimal dari byte adalah   : " + Byte.MAX_VALUE);
+        System.out.println("Nilai minimal dari byte adalah    : " + Byte.MIN_VALUE);
+        System.out.println("Besar byte dalam Byte adalah      : " + Byte.BYTES + " bytes");
+        System.out.println("Besar byte dalam Bit adalah       : " + Byte.SIZE + " bit");
+
+        System.out.println("     ");
+        System.out.println("======================================");
+        System.out.println("========         Short        ========");
+        System.out.println("======================================");
+        short s = 10;
+        System.out.println("Nilai dari short a adalah          : " + s);
+        System.out.println("Nilai maksimal dari short adalah   : " + Short.MAX_VALUE);
+        System.out.println("Nilai minimal dari short adalah    : " + Short.MIN_VALUE);
+        System.out.println("Besar short dalam Byte adalah      : " + Short.BYTES + " bytes");
+        System.out.println("Besar short dalam Bit adalah       : " + Short.SIZE + " bit");
+
+        System.out.println("     ");
+        System.out.println("======================================");
+        System.out.println("========          Long        ========");
+        System.out.println("======================================");
+        long l = 10;
+        System.out.println("Nilai dari Long a adalah          : " + l);
+        System.out.println("Nilai maksimal dari Long adalah   : " + Long.MAX_VALUE);
+        System.out.println("Nilai minimal dari Long adalah    : " + Long.MIN_VALUE);
+        System.out.println("Besar Long dalam Byte adalah      : " + Long.BYTES + " bytes");
+        System.out.println("Besar Long dalam Bit adalah       : " + Long.SIZE + " bit");
+
+        System.out.println("     ");
+        System.out.println("======================================");
+        System.out.println("========         Double        ========");
+        System.out.println("======================================");
+        double d = 10;
+        System.out.println("Nilai dari double a adalah          : " + d);
+        System.out.println("Nilai maksimal dari double adalah   : " + Double.MAX_VALUE);
+        System.out.println("Nilai minimal dari double adalah    : " + Double.MIN_VALUE);
+        System.out.println("Besar double dalam Byte adalah      : " + Double.BYTES + " bytes");
+        System.out.println("Besar double dalam Bit adalah       : " + Double.SIZE + " bit");
+
+        System.out.println("     ");
+        System.out.println("======================================");
+        System.out.println("========         Float        ========");
+        System.out.println("======================================");
+        float f = 10;
+        System.out.println("Nilai dari byte a adalah          : " + f);
+        System.out.println("Nilai maksimal dari byte adalah   : " + Float.MAX_VALUE);
+        System.out.println("Nilai minimal dari byte adalah    : " + Float.MIN_VALUE);
+        System.out.println("Besar byte dalam Byte adalah      : " + Float.BYTES + " bytes");
+        System.out.println("Besar byte dalam Bit adalah       : " + Float.SIZE + " bit");
+
+        System.out.println("     ");
+        System.out.println("======================================");
+        System.out.println("========          Char        ========");
+        System.out.println("======================================");
+        char c = 'A';
+        System.out.println("Nilai dari Char a adalah          : " + c);
+        System.out.println("Nilai maksimal dari Char adalah   : " + Character.MAX_VALUE);
+        System.out.println("Nilai minimal dari Char adalah    : " + Character.MIN_VALUE);
+        System.out.println("Besar Char dalam Byte adalah      : " + Character.BYTES + " bytes");
+        System.out.println("Besar Char dalam Bit adalah       : " + Character.SIZE + " bit");
+
+        System.out.println("     ");
+        System.out.println("======================================");
+        System.out.println("========        Boolean       ========");
+        System.out.println("======================================");
+        boolean b = true;
+        System.out.println("Nilai dari Char a adalah          : " + b);
+        System.out.println("Nilai maksimal dari Char adalah   : " + Boolean.FALSE);
+        System.out.println("     ");
+        System.out.println("     ");
+    }
+}
